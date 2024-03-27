c57d62cb3b7b00b35770a0de51507b29801dc64c
Kai Kewley <kaikewley@gmail.com>
2024-03-27 00:14:02 +0000

    Nginx install - fix file permissions.

--- ubuntu_20.04_LTS/install_nginx.sh
+++ ubuntu_20.04_LTS/install_nginx.sh
@@ -10,7 +10,7 @@ sudo apt-get -y install nginx
 sudo rm -rf /etc/nginx/sites-enabled/default
 
 # Add Nginx config for Snowstorm
-sudo curl 'https://raw.githubusercontent.com/IHTSDO/snowstorm-deploy/main/ubuntu_20.04_LTS/snowstorm-nginx.conf' > /etc/nginx/conf.d/snowstorm.conf
+curl 'https://raw.githubusercontent.com/IHTSDO/snowstorm-deploy/main/ubuntu_20.04_LTS/snowstorm-nginx.conf' | sudo tee /etc/nginx/conf.d/snowstorm.conf
 
 # Start Nginx
 sudo nginx
